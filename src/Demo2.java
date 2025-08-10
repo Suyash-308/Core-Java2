068f1e069d71e12f64d05b0c7c9092f9f5d768fb
Suyashnichit308 <nichitsuyash@gmail.com>
2025-08-10 23:00:30 +0530

    added reverse array

---
+++ src/Demo2.java
@@ -0,0 +1,18 @@
+public class Demo2 {
+    public static void main (String[]  args){
+        int[] array={10,22,13,18,52};
+
+      int left=0,right=array.length-1;
+      while (left < right){
+          int temp=array[left];
+          array [left]=array[right];
+          array [right]=temp;
+          left++;
+          right--;
+      }
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
