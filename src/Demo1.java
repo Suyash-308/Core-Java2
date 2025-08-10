5cf5722aed9b4f17186dc43f1d36d3b997fdd7b6
Suyashnichit308 <nichitsuyash@gmail.com>
2025-08-10 23:13:16 +0530

    added from array

---
+++ src/Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1 {
+    public static void main (String[] args){
+        int[] array={34,65,12,80,65,77};
+
+        System.out.println(array[0]);
+        System.out.println(array[array.length-2]);
+        System.out.println(array[2]);
+
+    }
+}
